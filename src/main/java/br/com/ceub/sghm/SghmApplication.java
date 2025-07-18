95a51f97a3fa774ee23b0fbb29fad08f00cdecb7
dupontcam <125046483+dupontcam@users.noreply.github.com>
2025-07-18 19:47:48 -0300

    Initial commit

---
+++ src/main/java/br/com/ceub/sghm/SghmApplication.java
@@ -0,0 +1,13 @@
+package br.com.ceub.sghm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SghmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SghmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ceub/sghm/SghmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ceub.sghm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SghmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
