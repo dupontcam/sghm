5bbed8cea2a28fc512fa5eba119debb6da5c2974
dupontcam <125046483+dupontcam@users.noreply.github.com>
2025-07-19 10:48:34 -0300

    User Role Model

---
+++ src/main/java/br/com/ceub/sghm/entities/User.java
@@ -0,0 +1,103 @@
+package br.com.ceub.sghm.entities;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+
+  @Column(unique = true)
+  private String email;
+  private String password;
+  
+
+  @ManyToMany
+      @JoinTable(name = "tb_user_role",
+              joinColumns = @JoinColumn(name = "user_id"),
+              inverseJoinColumns = @JoinColumn(name = "role_id"))
+      private Set<Role> roles = new HashSet<>();
+
+  public User() {
+  }
+
+  public User(Long id, String name, String password, String email) {
+    this.id = id;
+    this.name = name;
+    this.password = password;
+    this.email = email;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return name;
+  }
+
+  public void setUsername(String name) {
+    this.name = name;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+public void addRole(Role role) {
+    roles.add(role);
+  }
+
+  public boolean hasRole(String roleName) {
+    for (Role role : roles) {
+      if (role.getAuthority().equals(roleName)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null || getClass() != obj.getClass()) return false;
+    User user = (User) obj;
+    return Objects.equals(id, user.id);
+  }
+}
