7e0207c29b9005d9959eeedf26fd576778ac63b8
dupontcam <125046483+dupontcam@users.noreply.github.com>
2025-10-21 21:39:57 -0300

    Create reusable Layout and Sidebar components

---
+++ src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Sidebar from './Sidebar';
+import './Layout.css';
+
+const Layout: React.FC = () => {
+  return (
+    <div className="layout-container">
+      <Sidebar />
+      <main className="content-area">
+        <Outlet /> 
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
